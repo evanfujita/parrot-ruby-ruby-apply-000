2284f5005334590465e1a165cec2f5c0bac2753f
magical-tutorial-4913 <evanfujita@gmail.com>
2020-09-30 04:57:16 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot (Pretty Bird = "Squawk!")
+def parrot (bird = "Squawk!")
   puts "#{bird}"
   "#{bird}"
 end
 
+parrot
